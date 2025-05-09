9d7e936a5605052314e649ba0a0c4265470c573b
jeadie <jack@spice.ai>
2025-05-09 15:25:40 +1000

    initial improvements to python ADBC

--- clients/adbc/main.py
+++ clients/adbc/main.py
@@ -5,11 +5,21 @@ with connect(
     "grpc://127.0.0.1:50051",
 ) as conn:
     with conn.cursor() as cur:
-        cur.execute("SELECT $1 + 1 AS the_answer", parameters=(41,))
+        cur.execute("""
+            SELECT AccountId,ServiceId,AddOnSid,AddOnTypeSid,AddOnJson,DateCreated,DateUpdated
+            FROM addons
+            WHERE ServiceId LIKE $1
+            LIMIT $2
+            """, parameters=("s%9", 3))
         table = cur.fetch_arrow_table()
         print(table)
 
-        cur.execute("SELECT 1 AS one")
+        cur.execute("""
+            SELECT AccountId,ServiceId,AddOnSid,AddOnTypeSid,AddOnJson,DateCreated,DateUpdated
+            FROM addons
+            WHERE ServiceId=:id_pattern
+            LIMIT :batchSize
+            """, parameters={"id_pattern": "service123"}) # "batchSize": 5}) # "id_pattern": "s%9",   WHERE ServiceId LIKE :id_pattern
         table = cur.fetch_arrow_table()
         print(table)
 
